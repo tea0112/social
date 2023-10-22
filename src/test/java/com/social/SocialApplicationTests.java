ff7db0118d0a644095518bc3215a0f9f5b7aecde
tea0112 <ducthaidev@gmail.com>
2023-10-22 18:02:07 +0700

    remove unused test

--- src/test/java/com/social/SocialApplicationTests.java
+++ src/test/java/com/social/SocialApplicationTests.java
@@ -1,13 +1,7 @@
 package com.social;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SocialApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
